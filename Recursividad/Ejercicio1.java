283027369d328e7308b3317b35ce481a320c95f0
maxiteyo <maxiiteyo@gmail.com>
2023-08-24 11:11:48 -0300

    Cambio 2

--- Recursividad/Ejercicio1.java
+++ Recursividad/Ejercicio1.java
@@ -11,6 +11,7 @@ public static void main(String[] args) {
     
     int num1=0;
     int num2=0;
+    int resultado=0;
     int opcion=0;
     Mente miMente= new Mente();
 
@@ -18,30 +19,42 @@ public static void main(String[] args) {
 
 do {
     
-    System.out.println("\n-----MENU-----\n");
-    System.out.println("\n1- Ejercicio 1");
-    System.out.println("\n2- Ejercicio 2");
-    System.out.println("\n3- Ejercicio 3");
-    System.out.println("\n4- Ejercicio 4");
-    System.out.println("\n5- Ejercicio 5");
-    System.out.println("\n6- Ejercicio 6");
-    System.out.println("\n7- Ejercicio 7");
-    System.out.println("\n8- Salir");
+    System.out.println("-----MENU-----");
+    System.out.println("1- Ejercicio 1");
+    System.out.println("2- Ejercicio 2");
+    System.out.println("3- Ejercicio 3");
+    System.out.println("4- Ejercicio 4");
+    System.out.println("5- Ejercicio 5");
+    System.out.println("6- Ejercicio 6");
+    System.out.println("7- Ejercicio 7");
+    System.out.println("8- Salir");
     System.out.println("--------------");
+    System.out.print("OPCION: ");
     opcion=consola.nextInt();
 
     switch(opcion){
 
         case 1:
-
+        System.out.println("Ingrese un numero: ");
+        num1=consola.nextInt();
+        resultado=miMente.ej1(num1);
+        System.out.println("El factorial de "+num1+" es "+resultado);
         break;
 
         case 2:
-
+        System.out.println("Ingrese un numero: ");
+        num1=consola.nextInt();
+        resultado=miMente.ej2(num1);
+        System.out.println("La suma de los primero n enteros de "+num1+" es "+resultado);
         break;
 
         case 3:
-
+        System.out.println("Ingrese un numero: ");
+        num1=consola.nextInt();
+        System.out.println("Ingrese el exponente para elevar el numero: ");
+        num2=consola.nextInt();
+        resultado=miMente.ej3(num1,num2);
+        System.out.println(num1+" elevado a "+num2+" es "+resultado);
         break;
 
         case 4:
--- Recursividad/Mente.java
+++ Recursividad/Mente.java
@@ -14,16 +14,46 @@ public Mente(){
 
 }
 
+
 public int ej1(int n1){
 
 int resultado=0;
 
-    do {
+if(n1==0 || n1==1){
+    return 1;
+}
+else{
+    resultado=n1*ej1(n1-1);
+}
 
-    resultado=ej1(n1)
-} while (n1);
+
+return resultado;
 
 }
 
+public int ej2(int n1){
+
+int resultado=0;
+
+if(n1==0){
+    return 0;
+}
+resultado=n1+ej2(n1-1);
+
+return resultado;
+}
+
+
+public int ej3(int n1,int n2){
+
+int resultado=0;
+
+if(n2==0){
+    return 1;
+}
+resultado=n1*ej3(n1,n2-1);
+
+return resultado;
+}
 
 }
