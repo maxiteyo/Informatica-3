a58a4be4fc6e0563e5f71f68575e563b145b5a15
maxiteyo <maxiiteyo@gmail.com>
2023-08-24 09:50:01 -0300

    Cambio 1

---
+++ Recursividad/Ejercicio1.java
@@ -0,0 +1,69 @@
+package Recursividad;
+import java.util.Scanner;
+
+
+public class Ejercicio1{
+
+
+
+
+public static void main(String[] args) {
+    
+    int num1=0;
+    int num2=0;
+    int opcion=0;
+    Mente miMente= new Mente();
+
+    Scanner consola= new Scanner(System.in);
+
+do {
+    
+    System.out.println("\n-----MENU-----\n");
+    System.out.println("\n1- Ejercicio 1");
+    System.out.println("\n2- Ejercicio 2");
+    System.out.println("\n3- Ejercicio 3");
+    System.out.println("\n4- Ejercicio 4");
+    System.out.println("\n5- Ejercicio 5");
+    System.out.println("\n6- Ejercicio 6");
+    System.out.println("\n7- Ejercicio 7");
+    System.out.println("\n8- Salir");
+    System.out.println("--------------");
+    opcion=consola.nextInt();
+
+    switch(opcion){
+
+        case 1:
+
+        break;
+
+        case 2:
+
+        break;
+
+        case 3:
+
+        break;
+
+        case 4:
+
+        break;
+
+        case 5:
+
+        break;
+
+        case 6:
+
+        break;
+
+        case 7:
+
+        break;
+
+    }
+
+} while (opcion != 8);
+
+}
+
+}
---
+++ Recursividad/Mente.java
@@ -0,0 +1,29 @@
+package Recursividad;
+
+public class Mente {
+    
+private int n1;
+private int n2;
+
+public Mente(int n1, int n2) {
+    this.n1 = n1;
+    this.n2 = n2;
+}
+
+public Mente(){
+
+}
+
+public int ej1(int n1){
+
+int resultado=0;
+
+    do {
+
+    resultado=ej1(n1)
+} while (n1);
+
+}
+
+
+}
